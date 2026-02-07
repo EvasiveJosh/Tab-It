1850a7b79f443e383152a86ca8bc47de0ba86af2
Rory Macdonald <rorsmacd@gmail.com>
2026-02-07 18:42:41 -0500

    updated req

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from extraction import quantize_and_extract_notes
-from extraction import generate_simple_tab
-
 
 # example usage
-# notes = quantize_and_extract_notes("test.mid")
+notes = quantize_and_extract_notes("test.mid")
+print(notes)
